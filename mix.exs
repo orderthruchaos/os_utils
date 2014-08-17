90547aa11c4c2f5d131b859bf4ecd95c5555f189
Brett DiFrischia <orderthruchaos@gmail.com>
2014-08-17 11:42:22 -0500

    Adding Hex.pm metadata.

--- mix.exs
+++ mix.exs
@@ -1,11 +1,31 @@
 defmodule OsUtils.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @github  "https://github.com/orderthruchaos/os_utils"
+
   def project do
-    [app: :os_utils,
-     version: "0.1.0",
-     elixir: ">= 0.15.0",
-     deps: deps]
+    [
+      app: :os_utils,
+      version: @version,
+      elixir: ">= 0.15.0",
+      deps: deps,
+      package: [
+        contributors: ["Brett DiFrischia"],
+        licenses: ["Apache 2.0"],
+        links: [
+          {"Github", @github}
+        ]
+      ],
+      description: "OS utilities for Elixir."
+      # ,
+      # docs: [
+      #   readme: true,
+      #   main: "README",
+      #   source_url: @github,
+      #   source_ref: @version
+      # ]
+    ]
   end
 
   # Configuration for the OTP application
