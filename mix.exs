0474bfde2ba9cbb911f53286ae4d48ec0225707f
Brett DiFrischia <orderthruchaos@gmail.com>
2014-08-17 11:26:36 -0500

    Reorg.

--- mix.exs
+++ mix.exs
@@ -22,7 +22,5 @@ defmodule OsUtils.Mixfile do
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type `mix help deps` for more examples and options
-  defp deps do
-    []
-  end
+  defp deps, do: []
 end
