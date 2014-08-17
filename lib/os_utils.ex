9724524833fc9c304e8973b266a0e303c8b86f21
Brett DiFrischia <orderthruchaos@gmail.com>
2014-08-17 11:05:50 -0500

    Adding functions and tests.

--- lib/os_utils.ex
+++ lib/os_utils.ex
@@ -1,2 +1,20 @@
-defmodule OsUtils do
+defmodule OSUtils do
+  def is_win?,  do: os_id === :win32 || os_id === :win64
+  def is_unix?, do: os_id === :unix
+  def is_ose?,  do: os_id === :ose
+
+
+  def os_id, do: :os.type |> elem 0
+
+
+  def os_name, do: :os.type |> elem 1
+
+
+  def supports_symlinks? do
+    case :file.read_link(Path.expand(__ENV__.file)) do
+      {:error, :enotsup} -> false
+      _                  -> true
+    end
+  end
+
 end
--- test/os_utils_test.exs
+++ test/os_utils_test.exs
@@ -1,7 +1,48 @@
-defmodule OsUtilsTest do
+defmodule OSUtilsTest do
   use ExUnit.Case
+  alias OSUtils, as: OU
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+  test "is windows?" do
+    case :os.type do
+      {:win32, _} -> assert OU.is_win?
+      _           -> refute OU.is_win?
+    end
   end
+
+  test "is unix?" do
+    case :os.type do
+      {:unix, _} -> assert OU.is_unix?
+      _          -> refute OU.is_unix?
+    end
+  end
+
+  # Note:  needs tested on OSE.
+  test "is ose?" do
+    case :os.type do
+      {:ose, _} -> assert OU.is_ose?
+      _         -> refute OU.is_ose?
+    end
+  end
+
+  # Note:  needs tested on OSE.
+  test "os id" do
+    { os, _ } = :os.type
+    assert os == OU.os_id
+  end
+
+  # Note:  needs tested on OSE.
+  test "OS name" do
+    { _, name } = :os.type
+    assert name == OU.os_name
+  end
+
+  # TODO:  Test on system without symlink support.
+  test "has symlinks?" do
+    case :file.read_link(Path.expand(__ENV__.file)) do
+      {:error, :enotsup} -> refute OU.supports_symlinks?
+      _                  -> assert OU.supports_symlinks?
+    end
+  end
+
 end
