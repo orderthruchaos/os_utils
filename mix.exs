c3ef157b4c550edd2dc85dcc09a94df671086030
Brett N. DiFrischia <orderthruchaos@gmail.com>
2016-02-12 16:38:23 -0600

    Updating version number and cleaning up mix.exs file.

--- mix.exs
+++ mix.exs
@@ -1,14 +1,14 @@
 defmodule OSUtils.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @github  "https://github.com/orderthruchaos/os_utils"
 
   def project do
     [
       app: :os_utils,
       version: @version,
-      elixir: ">= 1.0.0",
+      elixir: ">= 1.2.0",
       deps: deps,
       description: "OS utilities for Elixir.",
       package: [
@@ -25,20 +25,8 @@ defmodule OSUtils.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application, do: []
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:ex_doc, "~> 0.5", only: :dev}
